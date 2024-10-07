18a8df831cb258b935dd75b77c576c4424523ae7
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-07 00:17:41 -0300

    Create questao03.c
    tem que completar a resposta

---
+++ questao03.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+  int i, j, *p, *q;
+  
+  p = &i; 
+  // Linha 3 - P não foi declarado
+  
+  *q = &j;
+  // linha 6 - q não foi declarado, além disso *q guardaria apenas valores interios, enquanto &J é um endereço.
+  
+  p = &*&i;
+  // linha 9 
+  
+  i = (*&)j;
+  // parenteses precisariam estar cobrindo o j
+  i = *&j;
+  
+  
+  i = *&*&j;
+  
+  q = *p;
+  // a variavel só armazena um endereço para inteiro
+  i = (*p)++ + *q;
+
+}

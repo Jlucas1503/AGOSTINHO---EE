6e6f73c63dfe7fa920965398e630a75ccc740808
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-07 23:15:48 -0300

    Revisar dps a resposta(parcialmente concluida)

---
+++ questao05.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+void funcao(char** str){
+  str++;
+}
+int main(){
+  char *str = (void *)malloc(50*sizeof(char));
+  strcpy(str, "gostinho");
+  funcao(&str);
+  puts(str);
+  free(str);
+  return 0;
+}

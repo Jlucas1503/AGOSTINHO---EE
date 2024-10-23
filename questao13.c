fe16e10994d2b3ff1f0baff64d5a89865fa3e213
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-23 12:02:25 -0300

    1/4 da questao feita apenas

---
+++ questao13.c
@@ -0,0 +1,18 @@
+/* respondendo a questão: 
+x+1, x+2, x+3
+
+Se  x for declarado como char:
+
+x + 1 = 4093
+x + 2 = 4094
+x + 3 = 4095
+
+Sex for declarado como int:
+
+x + 1 = 4094
+x + 2 = 4096
+x + 3 = 4098
+
+
+*/
+

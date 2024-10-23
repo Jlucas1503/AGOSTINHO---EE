b74b2e22ce2ac4c58da639409ed255ef936e26c0
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-23 11:54:22 -0300

    q completa, mas tem uma mensagem de WARNING

---
+++ questao12.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+ 
+ void main(){
+int const *x = 3; 
+ 
+ 
+ 
+// o tipo de variavel const faz com que a variavel se torne constante, ou seja, não possa ser alterada 
+
+
+ printf("%d", ++(*x)); // erro, por x ser uma variavel constante, ela n pode receber o incremente ++
+ 
+ }

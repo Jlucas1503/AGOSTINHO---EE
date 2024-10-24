828bce6abd35c570a0f53067e9dd7e05adf784c3
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-24 17:31:42 -0300

    completo

--- questao15.c
+++ questao15.c
@@ -7,3 +7,10 @@ int main(){
   
   return 0;
 }
+
+/* o resultado da compilação do programa é: 
+1 - o endereço do primeiro elemento da linha quatro(x[3][0]);
+2 - colocando o asterisco antes do parenteses, estamos "desapontando" nossa matriz uma vez, ou seja
+, nos mostra o primeiro elemento da linha quatro, coincidentemente igual ao primeiro elemento do print.
+
+3 - ele "desaponta" a matriz uma vez, que nos levaria ao endereço de x[2][0], e depois soma mais 3, ou seja, pula tres endereços, que mais uma vez coincide com os outros dois*/    

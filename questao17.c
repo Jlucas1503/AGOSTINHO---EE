033985cc257e9f6464a3cd2d04692909f9ea76bd
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-25 19:08:14 -0300

    Create questao17.c

---
+++ questao17.c
@@ -0,0 +1,12 @@
+/* resposta da questao
+Um memory leak ocorre quando um programa reserva memória usando malloc mas depois esquece de liberar essa memória quando termina a execução da alocação(usando o free). Isso significa que a memória fica alocada, mas o programa não tem mais acesso a ela, causando desperdício de memória. Se isso acontecer repetidamente, pode levar o programa a consumir toda a memória disponível. 
+
+
+
+
+
+
+
+
+
+*/

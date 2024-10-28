cbe69cf2fc778f1e4a7995ce383a1b6b44ba029f
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-27 21:33:23 -0300

    finalizado

--- questao03.c
+++ questao03.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 int main(){
-  int i, j, *p, *q;
+  int i = 3, j = 5, *p, *q; // variaveis que eu mesmo atribuí, não sao originais do programa
   
-  p = &i; 
-  // Linha 3 - P não foi declarado
+  p = &i; // correto
   
   *q = &j;
-  // linha 6 - q não foi declarado, além disso *q guardaria apenas valores interios, enquanto &J é um endereço.
+  // *q guardaria apenas valores interios, enquanto &J é um endereço. erro 
   
   p = &*&i;
-  // linha 9 
+  // *&i indicaria o proprio i, então quando eu adiciona um & antes, é a mesma coisa de escrever o endereço de i(&i)
+  printf("P = %d\n", *p);
+  
   
   i = (*&)j;
-  // parenteses precisariam estar cobrindo o j
-  i = *&j;
+  // parenteses precisariam estar cobrindo o j. esse comando simplesmente nao é reconhecido pelo c
+
+  
+  i = *&j; //essa seria a maneira correta de desreferenciar o endereço de j.
+  printf("i = %d\n", i);
   
   
-  i = *&*&j;
+  i = *&*&j; // correto, *&*&j é mesma coisa de escrever apenas j
+  printf("i = %d\n", i);
   
   q = *p;
-  // a variavel só armazena um endereço para inteiro.
-  i = (*p)++ + *q;
+  // a variavel só armazena um endereço de inteiro. ERRO
+  
+  i = (*p)++ + *q; // correto apenas se q estiver apontando tambem para um valor inteiro
+  
 
 }

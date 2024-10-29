bafa8ec3335e0c5acf924fa85449edf01b43a5bc
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-28 23:50:29 -0300

    Finalizado - so revisar

--- questao18.c
+++ questao18.c
@@ -1,18 +1,31 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+/*
+Um ponteiro para função em C é uma variável que armazena o endereço de uma função. Isso permite que a função seja chamada de forma indireta através do ponteiro, o que pode ser útil em várias situações*/
 
-void funcao(char** str) {
-    // Agora estamos alterando o ponteiro original
-    (*str)++;
+
+
+#include <stdio.h>
+
+void saudacao1() {
+    printf("Hello world!\n");
+}
+
+void saudacao2() {
+    printf("Meu codigo aaaa\n");
 }
 
 int main() {
-    char *str;
-    str = (void *)malloc(50 * sizeof(char));  // Aloca memória para a string
-    strcpy(str, "Agostinho");  // Copia "Agostinho" para a string alocada
-    funcao(&str);  // Chama a função e altera o ponteiro original para apontar para "gostinho"
-    puts(str);  // Agora imprime "gostinho"
-    free(str);  // Libera a memória alocada
+    // ponteiro para função que não recebe parâmetros e não retorna valor
+    void (*ponteiroParaSaudacao)() = NULL;
+
+    // Atribui a função saudacao1 ao ponteiro
+    ponteiroParaSaudacao = saudacao1;
+    ponteiroParaSaudacao();  // Chama a função através do ponteiro
+
+    // Atribui a função saudacao2 ao ponteiro
+    ponteiroParaSaudacao = saudacao2;
+    ponteiroParaSaudacao();  // Chama a função através do ponteiro
+
     return 0;
+
 }
+    

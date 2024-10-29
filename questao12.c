d1a97cacee43e57e12ed763072b748184f57b06a
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-28 23:22:30 -0300

    Finalizado - so revisar

--- questao12.c
+++ questao12.c
@@ -5,7 +5,7 @@ int const *x = 3;
  
  
  
-// o tipo de variavel const faz com que a variavel se torne constante, ou seja, não possa ser alterada 
+// o tipo de variavel const faz com que a variavel se torne constante, ou seja, não possa ser alterada  
 
 
  printf("%d", ++(*x)); // erro, por x ser uma variavel constante, ela n pode receber o incremento ++

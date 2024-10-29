604c5b3748e82ca7021533af12fa19a44ef091c5
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-28 23:10:41 -0300

    Finalizado - so revisar

--- questao18.c
+++ questao18.c
@@ -1,1 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void funcao(char** str) {
+    // Agora estamos alterando o ponteiro original
+    (*str)++;
+}
+
+int main() {
+    char *str;
+    str = (void *)malloc(50 * sizeof(char));  // Aloca memória para a string
+    strcpy(str, "Agostinho");  // Copia "Agostinho" para a string alocada
+    funcao(&str);  // Chama a função e altera o ponteiro original para apontar para "gostinho"
+    puts(str);  // Agora imprime "gostinho"
+    free(str);  // Libera a memória alocada
+    return 0;
+}

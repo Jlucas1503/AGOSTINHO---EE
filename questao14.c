dc76b9e6bec7d3b88e6b741cc2f7ca2551ecc80f
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-10-28 23:32:32 -0300

    Finalizado

--- questao14.c
+++ questao14.c
@@ -7,7 +7,7 @@
 int f(int a, int *pb, int **ppc) { // a função tem como parametros: a(que assume o valor de c), *pb(que assume o endereço apontado por b) e **ppc(que assume o ponteiro para ponteiro a)
   int b, c;
   
-  **ppc += 1; // aqui acontece um incremento. ppc assume a variavel a da função main, que por sua vez aponta para o endereço de outro ponteiro(b), que por sua vez aponta pra o endereço de c.
+  **ppc += 1; // aqui acontece um incremento. ppc assume a variavel "a" da função main, que por sua vez aponta para o endereço de outro ponteiro(b), que por sua vez aponta pra o endereço de c.
   
   printf("ppc:%d\n", **ppc);
   
@@ -15,13 +15,13 @@ int f(int a, int *pb, int **ppc) { // a função tem como parametros: a(que assu
   
   
   
-  *pb += 2; // pb assume b, que na função main aponta para o endereço de a. Portanto na linha, *pb = 5(pois estou mostrando o conteudo da variavel apontada)
+  *pb += 2; // pb assume "b" da função main, que aponta para o endereço de c. Portanto na linha, *pb = 8(c que agora é 6, + 2 = 8)
   
   printf("pb:%d\n", *pb);
   
-  b = *pb;
+  b = *pb; //b assume o novo valor de pb que é 8
   
-  a += 3;
+  a += 3; // a assume o valor de c na função main, e soma 3, logo a = 8
   
   return a + b + c;
 }

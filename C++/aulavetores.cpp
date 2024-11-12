961f1d8a2ac27e6441822439bed0cdac302f82c1
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-11-12 16:28:49 -0300

    Update aulavetores.cpp

--- C++/aulavetores.cpp
+++ C++/aulavetores.cpp
@@ -1,4 +1,6 @@
 #include <iostream> 
+#include <cmath>
+
 
 class Vetor{
     private:
@@ -14,15 +16,36 @@ class Vetor{
         
             
         }
+        
+        void SetY(float y_){ // cria um metodo Set que recebe uma variavel x_
+        
+            y = y_; // atribui o valor de x_ a variavel x 
+        
+            
+        }
+        
 
-        float getX(){ // o metodo Get recupera o valor da variavel x
+        float getX(){ /* o metodo Get recupera o valor da variavel x | a funcao Get não precisa de um valor/parametro */
             return x; 
+        }
+        
+        
+        float getY(){ /* o metodo Get recupera o valor da variavel x | a funcao Get não precisa de um valor/parametro */
+            return y; 
+        }
+
+    
+        float norma(){
+            return sqrtf(pow(x,2) + pow(y,2));       
+
         }
 
 
+      //  float angulo(){}
 
 
 
+     //   Vetor unitario(){}
 };
 
 
@@ -31,12 +54,13 @@ int main(){
     Vetor v;                               // cria um objeto dentro da classe vetor
 
 
-    v.SetX(3);                              // chama o metodo Set, que "Seta" o valor tres na propriedade x do Objeto v
+    v.SetX(3);// chama o metodo Set, que "Seta" o valor tres na propriedade x do Objeto v
     
+    v.SetY(4);
     
-    std::cout << v.getX();          // mostra a saida do get
+    //std::cout << v.getX();          // mostra a saida do get
 
-
+    v.norma();
 
     return 0;
 }

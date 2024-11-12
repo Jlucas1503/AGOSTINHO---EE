c9410a8407987f09dfae39640637cc13114f8b33
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-11-12 15:33:00 -0300

    teste
    testando qualquer coisa

---
+++ C++/aulaVetores/teste.cpp
@@ -0,0 +1,38 @@
+#include <iostream>{ 
+
+class vetor{
+    private:
+        float x, y;
+
+
+
+
+    public:
+        void SetX(float x_){
+            x = x_;
+        }
+
+        float getX(){
+            return x; 
+        }
+
+
+
+
+
+};
+
+
+
+int main(){
+    vetor v;                               // cria um objeto dentro da classe vetor
+
+
+    v.x = 3
+
+
+
+    return 0;
+}
+
+

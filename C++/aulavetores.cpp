e6476dc042bffa3424140420eab2dd8bef4252cd
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-11-14 15:36:18 -0300

    Add funcoes novas
    Adicionei as funçoes norma, angulo e vetor unitario.
    Além disso, comentei alguma coisas.

--- C++/aulavetores.cpp
+++ C++/aulavetores.cpp
@@ -33,35 +33,72 @@ class Vetor{
         float getY(){ /* o metodo Get recupera o valor da variavel x | a funcao Get não precisa de um valor/parametro */
             return y; 
         }
+        
+        
+        void print(){
+            std::cout << "(" << x << "," << y << ")" << std::endl;
+            
+            
+        }
 
     
         float norma(){
-            return sqrtf(pow(x,2) + pow(y,2));       
+            return sqrtf(x * x + y * y);       
 
         }
 
 
-      //  float angulo(){}
+        float angulo(){
+            return atan2f(y,x);
+        }
+            
 
 
-
-     //   Vetor unitario(){}
+        Vetor unitario(){
+            Vetor unit;
+            
+            unit.x = x / norma();
+            
+            unit.y = y / norma();
+            
+            
+            
+            
+            return unit;
+        }
 };
 
 
 
 int main(){
-    Vetor v;                               // cria um objeto dentro da classe vetor
+    Vetor v, u;    // cria um objeto dentro da classe vetor
 
 
-    v.SetX(3);// chama o metodo Set, que "Seta" o valor tres na propriedade x do Objeto v
+    v.SetX(3);  // chama o metodo Set, que "Seta" o valor tres na propriedade x do Objeto v
     
     v.SetY(4);
     
     //std::cout << v.getX();          // mostra a saida do get
 
-    v.norma();
+   std:: cout <<"norma é: "  << v.norma() << std:: endl;    // chama a função norma
+
+    std:: cout <<"arco tangente em rad é: "  << v.angulo() << std:: endl;
+
+
+    /*std:: cout <<"vetor unitario é: "  << v.unitario() << std:: endl; // não funciona, pois cout só mostra int, char e seus derivados
+    , não mostra um valor FLOAT*/
+
+    u = v.unitario();
+    
+    u.print();
+    
+    
+    
 
     return 0;
 }
+
+
+
+
 
